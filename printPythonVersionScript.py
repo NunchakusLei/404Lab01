0b25ebae7d54824270cf8d2fc58c73f8c8f62f12
NunchakusLei <chenrui@ualbera.ca>
2017-01-11 18:03:53 -0700

    modify the python check version script

--- printPythonVersionScript.py
+++ printPythonVersionScript.py
@@ -4,6 +4,6 @@ import requests
 
 print requests.__version__
 
-response = requests.get("http://google.com/")
+response = requests.get("https://raw.githubusercontent.com/NunchakusLei/404Lab01/master/printPythonVersionScript.py")
 print response.text
 print response.status_code
